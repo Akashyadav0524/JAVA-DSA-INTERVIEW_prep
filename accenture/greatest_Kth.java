2b02bc8085d942476705b40bf7e007199dbc51bb
Akash <akashy0524@gmail.com>
2023-10-10 01:06:50 +0530

    Accenture

--- accenture/greatest_Kth.java
+++ accenture/greatest_Kth.java
@@ -27,3 +27,7 @@ public class greatest_Kth {
         return arr[arr.length-(k+1)];
     }
 }
+// 18. Create web access management to the kth largest number. It will accept an integer k and an array arr as its conditions and has to return the greatest element based on the value of k. That is, if k = 0, return the greatest element. If k = 1, return the second greatest element, and so on.
+// Example
+
+// Suppose the array contains values like {74, 85, 102, 99, 101, 56, 84} and the integer k is 2. The method will return 99, the third greatest element, as there are only two (according to the value of k) values greater than 99 (101 and 102).
