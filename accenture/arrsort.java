eb2f281291c0cc4b90203dba9a02095f988d62c2
Akash <akashy0524@gmail.com>
2023-10-13 23:33:57 +0530

    Accenture

--- accenture/arrsort.java
+++ accenture/arrsort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class arrsort {
     public static void main(String[] args) {
-        int arr[] ={3,6,7,4,9,1,5};
+        int arr[] ={3,6,5,8,7,4,9,1,5};
 
         Arrays.sort(arr);
         System.out.println(arr.toString());
