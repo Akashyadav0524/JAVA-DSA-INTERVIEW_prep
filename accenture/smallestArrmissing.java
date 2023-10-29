04b5504986f972cf7a52d67919bd8a545975cc1f
Akash <akashy0524@gmail.com>
2023-10-29 18:23:17 +0530

    Accenture

--- accenture/smallestArrmissing.java
+++ accenture/smallestArrmissing.java
@@ -15,20 +15,32 @@ public class smallestArrmissing {
         in.close();
     }
 
-    private static int findMissing(int[] arr) {
-        if(arr.length==0){
-            return 0;
-        }
-        Arrays.sort(arr);
-        for (int i = 0; i < arr.length; i++) {
-             if(i>0) {
-                int a=1;
-                if(i!=a){
-                    return a;
-                }
-                a++;
+ 
+        private static int findMissing(int[] arr) {
+            if (arr.length == 0) {
+                return 1; // If the array is empty, the smallest missing positive integer is 1.
             }
+    
+            Arrays.sort(arr); // Sort the array in ascending order.
+    
+            int smallestMissing = 1; // Initialize the smallest missing positive integer to 1.
+    
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] <= 0) {
+                    continue; // Skip non-positive numbers.
+                }
+    
+                // If the current element matches the expected smallestMissing, increment it.
+                if (arr[i] == smallestMissing) {
+                    smallestMissing++;
+                } else {
+                    // If the current element is greater than smallestMissing, we found the missing integer.
+                    return smallestMissing;
+                }
+            }
+    
+            // If we have iterated through the entire array, all positive integers from 1 to n are present.
+            // In this case, the smallest missing positive integer is n + 1.
+            return smallestMissing;
         }
-        return -1;
-    }
 }
