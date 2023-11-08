01a2408f1e5d6acdadd901805c6946db72b1ce1b
Akash <akashy0524@gmail.com>
2023-11-08 21:32:33 +0530

    Accenture

---
+++ accenture/ArrayInput.java
@@ -0,0 +1,17 @@
+package accenture;
+import java.util.Scanner;
+import java.util.Arrays;
+public class ArrayInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the elements of the array separated by space: ");
+        String input = sc.nextLine();
+        String[] strArr = input.split(" ");
+        int[] arr = new int[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            arr[i] = Integer.parseInt(strArr[i]);
+        }
+        System.out.println("Array elements are: " + Arrays.toString(arr));
+        sc.close();
+    }
+}
