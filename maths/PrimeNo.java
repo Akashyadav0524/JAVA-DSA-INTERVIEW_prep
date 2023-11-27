005c7712c0dfa936cbd78f8becd27c2311ca5019
Akash <akashy0524@gmail.com>
2023-11-27 11:47:15 +0530

    Accenture

--- maths/PrimeNo.java
+++ maths/PrimeNo.java
@@ -4,7 +4,7 @@ public class PrimeNo {
     public static void main(String[] args) {
         int n = 20;
         for (int i = 1; i <=n;  i++) {
-            System.out.println(i +  "" + isPrime(i));
+            System.out.println(i +  " " + isPrime(i));
 
         }
     }
